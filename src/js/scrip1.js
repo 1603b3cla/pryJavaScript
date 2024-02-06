1f9650fc64b63e5be708de55352d81af5099beed
EsperanzaBelen <80125396+EsperanzaBelen@users.noreply.github.com>
2024-02-06 13:51:33 -0600

    primeros cambios al proyecto

---
+++ src/js/scrip1.js
@@ -0,0 +1,20 @@
+/*alert("Hello");
+console.log('soy un mensaje independiente');
+
+
+let nombre = prompt("introduce tu nombre");
+console.log('tu nombre es', nombre);
+
+let x = 56;
+let y = 78;
+let g = 78;
+*/
+
+const location1 = "New York," + "NY";
+const location2 = "New York," + "NY";
+const location3 = "New York," + "NY";
+
+
+console.log(location1);
+console.log(location2);
+console.log(location3);
