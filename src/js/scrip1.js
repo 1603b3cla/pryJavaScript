16423045a2a59daa5444425ed5f519c634682d00
1603b3cla <calvoe53@gmail.com>
2024-02-13 13:13:49 -0600

    Segundo Comit

--- src/js/scrip1.js
+++ src/js/scrip1.js
@@ -8,7 +8,7 @@ console.log('tu nombre es', nombre);
 let x = 56;
 let y = 78;
 let g = 78;
-*/
+
 
 const location1 = "New York," + "NY";
 const location2 = "New York," + "NY";
@@ -18,3 +18,159 @@ const location3 = "New York," + "NY";
 console.log(location1);
 console.log(location2);
 console.log(location3);
+
+let valor1 = 45;
+let valor2 = "David";
+
+if(true){
+    let x = 89;
+
+}
+
+
+let nombre, edad, esposa;
+nombre = 'Edgar David';
+edad = 22;
+esposa = 'Belen';
+console.log("Mi nombre es " + nombre + " y tengo " + edad + " años, y mi princesa es " + esposa);
+*/
+
+/*Scope de var tiene ambito
+function varTest() {
+
+    var x = 31;
+    if (true) {
+        var x = 56;
+        console.log(x);
+    }
+    console.log(x);
+}
+varTest();
+
+
+function letTest() {
+
+    let x = 31;
+    if (true) {
+        let x = 56;
+        console.log(x);
+    }
+    console.log(x);
+}
+letTest();
+
+
+console.log(x);
+var x = 34;
+*/
+/*
+console.log(x);
+let x = 34;*/
+/*
+function ejemploAmbitoVar() {
+    if (true) {
+        const x = 23;
+    }
+    console.log(x);
+}
+
+ejemploAmbitoVar();*/
+
+/*
+
+let y = 23;
+y = 'Hola mundo';
+console.log(y);*/
+
+/*
+const v = 6;
+
+function constEjemplo() {
+    const v = 5;
+    if (true) {
+        const v = 70;
+        console.log(v);
+
+    }
+    console.log(v);
+}
+console.log(v);
+
+constEjemplo(); */
+
+
+/*
+
+
+//Tipo de datos de JavaScript
+
+let age = 22;
+let base = 4.5;
+let distanciaTierra_sol = 4323423423;
+let name = "Edgar David Calvo Garcia";
+let gender = 'M';
+let aprobado = afalse;
+let weight = undefined;
+let height = null;
+let resultFuction = function enviar() {};
+let symbol = 'a';
+let objPersona = {
+    nombre: 'Edgar David',
+    apellido: 'calvo'
+
+}
+let arrayNameFruits = ['pera', 'manzana', 'mandarina'];
+
+
+
+console.log('symbol:${typeof symbol}');
+
+
+let pesoCelular = 1.4;
+pesoCelular = 'Uno punto cuatro';*/
+
+
+
+//Cadena de caracteres
+
+/*
+
+let nameUser = 'Ejemplo = "edgar" ';
+let nameAdmin = "Ejemplo = 'edgar' ";
+
+console.log(`francesa:${nameUser} mas texto ${nameAdmin}`);
+console.log('francesa:', nameAdmin)
+console.log('francesa:' + ameAdmin)
+
+
+let name = "Ilya";
+alert(`helo $ {1}`);
+
+
+let nombreAnimal = 'Mono'; */
+
+function factorial(n) {
+    // Caso base: el factorial de 0 o 1 es 1
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        // Caso recursivo: n! = n * (n-1)!
+        return n * factorial(n - 1);
+    }
+}
+
+// Ejemplo de uso
+console.log(factorial(5)); // Imprimirá 120
+
+
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        // Caso recursivo: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+// Ejemplo de uso
+console.log(fibonacci(6)); // Imprimirá 8
